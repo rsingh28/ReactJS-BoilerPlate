e47183abe6f52ad3f15f514202868054bbfe3472
rsingh28 <rashmeet.singh28@gmail.com>
2017-12-02 23:47:36 -0500

    First Commit - A working setup to display Hello World

---
+++ src/app/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+
+	render(){
+		return(
+			<div>
+				<h1>Hello World!</h1>
+			</div>
+		);
+	}
+
+}
+
+render(<App/>, window.document.getElementById("test"));
+
+console.log("It works!");
